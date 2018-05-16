d04bcb017402202b43ad2311fecb6f72be550855
Ritaotao <ritaotao28@gmail.com>
2018-05-15 22:42:39 -0400

    Finished index table, brainstorm if latest table necessary

--- pymtattl/core.py
+++ pymtattl/core.py
@@ -75,13 +75,12 @@ def read_prior(url):
             err_count += 1
         else:
             # keys: ca/units
-            # + every 5: daten/timen/descn/entriesn/exitsn
+            # + every 5 add: daten/timen/entriesn/exitsn
             keys = cols[:2]
             for i in range(3, ncol, 5):
-                row = tuple(keys+cols[i:i+5])
+                row = tuple(keys+cols[i:i+2]+cols[i+3:i+5])
                 rows.append(row)
-    labels = ['booth', 'remote', 'date', 'time',
-              'description', 'entries', 'exits']
+    labels = ['booth', 'remote', 'date', 'time', 'entries', 'exits']
     df = pd.DataFrame.from_records(rows, columns=labels)
     print("%d invalid lines with wrong # columns" % err_count)
     return df
@@ -95,19 +94,29 @@ def read_current(url):
     """
     labels = ['booth', 'remote', 'scp', 'station', 'linename', 'division',
               'date', 'time', 'description', 'entries', 'exits']
-    use = [0, 1, 6, 7, 8, 9, 10]
+    use = [0, 1, 6, 7, 9, 10]
     df = pd.read_table(url, sep=',', header=0, names=labels, usecols=use)
     return df
 
 
-def load_tables(init=True, paths=None):
+def load_tables(init_tables=True):
+    """
+        Load or init index table and latest table,
+        index table (primary key for data table):
+            id, booth, remote;
+        latest table (as base numbers for following data file):
+            index_id, latest_time, raw_entries, raw_exits;
+        init_tables: boolean or list,
+            list of index table and latest table paths,
+            default to true, create init_tables
+    """
     id_dfcols = ['booth', 'remote']
-    lt_dfcols = ['index_id', 'date_time', 'raw_entries', 'raw_exits']
-    if isinstance(paths, list) and len(paths) == 2:
-        index_df = pd.read_table(paths[0], sep=',', index_col=0,
+    lt_dfcols = ['index_id', 'latest_time', 'raw_entries', 'raw_exits']
+    if isinstance(init_tables, list) and len(init_tables) == 2:
+        index_df = pd.read_table(init_tables[0], sep=',', index_col=0,
                                  header=0, names=id_dfcols)
-        latest_df = pd.read_table(paths[1], sep=',', names=lt_dfcols)
-    elif init is True:
+        latest_df = pd.read_table(init_tables[1], sep=',', names=lt_dfcols)
+    elif init_tables is True:
         index_df = pd.DataFrame(columns=id_dfcols)
         latest_df = pd.DataFrame(columns=lt_dfcols)
     else:
@@ -115,28 +124,46 @@ def load_tables(init=True, paths=None):
     return (index_df, latest_df)
 
 
-def read_urls(urls, init=True, paths=None):
-    index, latest = load_tables(init, paths)
-    for u in urls:
-        if utils.getPubDate(u) < 141018:
-            df = read_prior(u)
-        else:
-            df = read_current(u)
-        print("Load into dataframe, start processing...")
+def update_index(df, index_table):
+    """
+        update index table with new booth, remote pairs,
+        drop booth, remote columns, keep only index column from df
+    """
+    id_pairs = df[['booth', 'remote']].drop_duplicates()
+    index = index_table.append(id_pairs, ignore_index=True).drop_duplicates()
+    df = df.merge(index.reset_index(), on=['booth', 'remote'], how='left')
+    df.drop(['booth', 'remote'], axis=1, inplace=True)
+    df.rename(columns={'index': 'index_id'}, inplace=True)
+    return (df, index_table)
 
-        # convert to date_time and integer columns
-        df['date'] = df['date'].apply(utils.formatDate)
-        df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'])
-        df.drop(['date', 'time'], axis=1, inplace=True)
-        df['entries', 'exits'] = df['entries', 'exits'].applymap(int)
-        # update index table, replace booth,remote with index_id
-        idcols = ['booth', 'remote']
-        id_pairs = df[idcols].drop_duplicates()
-        index = index.append(id_pairs, ignore_index=True).drop_duplicates()
-        df = df.merge(index.reset_index(), on=idcols, how='left')
-        df.drop(idcols, axis=1, inplace=True)
-    index.to_csv('index_table.txt', index_label='id')
-    # question: do i really need scp level?
+
+def update_latest(df, latest_table):
+    """
+        update latest table with most recent raw numbers found in df
+        for each index (booth, remote pairs)
+        !!!pause: what if counter reset next week???
+    """
+    latest = df.loc[df.groupby('index_id')['date_time'].idxmax()]
+    df = df.merge(latest_table, on='index_id', how='left')
+    df[['raw_entries', 'raw_exits']].fillna(0, inplace=True)
+
+
+def read_url(url, index_table, latest_table):
+    if utils.getPubDate(url) < 141018:
+        df = read_prior(url)
+    else:
+        df = read_current(url)
+    print("Load into dataframe, start processing...")
+
+    # convert to date_time and integer columns
+    df['date'] = df['date'].apply(utils.formatDate)
+    df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+    df.drop(['date', 'time'], axis=1, inplace=True)
+    df['entries', 'exits'] = df['entries', 'exits'].applymap(int)
+
+    df, index_df = update_index(df, index_table)
+    df = df.groupby(['index_id', 'date_time'])['entries',
+                                               'exits'].sum().reset_index()
 
 
 if __name__ == "__main__":
