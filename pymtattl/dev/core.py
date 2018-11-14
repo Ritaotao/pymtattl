395a766cd4d881e5a687c9af2b45d63d32993784
Ritaotao <ritaotao28@gmail.com>
2018-11-14 16:40:27 -0500

    process single file done, next step look into table creation and index table use

--- pymtattl/dev/core.py
+++ pymtattl/dev/core.py
@@ -62,7 +62,8 @@ class Downloader:
     def __init__(self, date_range=('2010-05-05', '2010-05-15'),
                  main_path='./data/'):
         self._jobname = 'download'
-        self.start_date, self.end_date = date_range
+        self.date_range = date_range
+        self._start_date, self._end_date = None, None
         self.main_path = main_path
         self._output_path = None
         self._log_path = None
@@ -82,9 +83,10 @@ class Downloader:
         createLogger(self._jobname, self._log_path)
         logger = logging.getLogger(self._jobname)
         try:
-            self.start_date = str2intDate(self.start_date)
-            self.end_date = str2intDate(self.end_date)
-            assert self.start_date <= self.end_date, "Not a valid date range"
+            self._start_date = str2intDate(self.date_range[0])
+            self._end_date = str2intDate(self.date_range[1])
+            assert self._start_date <= self._end_date, "Not a valid date range"
+            logger.info("Download data between {0} and {1}".format(self._start_date, self._end_date))
         except Exception as e:     
             logger.error(e, exc_info=True)
             raise
@@ -110,7 +112,7 @@ class Downloader:
 
     def run(self):
         """execution phase based on parameters"""
-        self.urls = getDataAddress(self.start_date, self.end_date, self._jobname, None)
+        self.urls = getDataAddress(self._start_date, self._end_date, self._jobname, None)
         self._download()
         return self._output_path
 
@@ -124,12 +126,13 @@ class Cleaner:
             output_type: "sqlite"|"postgres",
             config_path (json file path including database connection parameters): dir(str)
     """
-    def __init__(self, date_range=("2010-05-05", "2010-05-15"),
+    def __init__(self, date_range=None,
                  input_path='./data/', output_type='sqlite',
                  dbparam=None):
         self._jobname = 'clean'
         self._bulk_process = 10
-        self.start_date, self.end_date = date_range
+        self.date_range = date_range
+        self._start_date, self._end_date = None, None
         self.input_path = input_path
         self.output_type = output_type
         self.dbparam = None
@@ -144,89 +147,93 @@ class Cleaner:
         createLogger(self._jobname, self._log_path)
         logger = logging.getLogger(self._jobname)
         try:
-            self.start_date = str2intDate(self.start_date)
-            self.end_date = str2intDate(self.end_date)
-            assert self.start_date <= self.end_date, "Not a valid date range"
+            if self.date_range:
+                self._start_date = str2intDate(self.date_range[0])
+                self._end_date = str2intDate(self.date_range[1])
+                assert self._start_date <= self._end_date, "Not a valid date range"
+                logger.info("Use data files between {0} and {1}".format(self._start_date, self._end_date))
+            else:
+                logger.info("No date range specified, use all data files in folder")
             assert self.output_type in ("sqlite","postgres"), "Output_type could only be sqlite, postgres"
         except Exception as e:     
             logger.error(e, exc_info=True)
             raise
         return True
 
+    def _processFile(self, file):
+        """process each data file, return pandas dataframe
+            a. check # columns
+            b. combine date and time column, convert to timestamp
+            c. convert entry/exit to integer type
+        """
+        logger = logging.getLogger(self._jobname)
+        datevalue = parseDate(file)
+        with open(os.path.join(self.input_path, file), 'r') as f:
+            data = f.readlines()
+        rows = []
+        for i, line in enumerate(data):
+            # post 141018, data file has header, skip first row
+            if datevalue >= 141018 and i == 0:   
+                continue
+            line = line.replace('\x00', '')
+            cols = line.strip().split(',')
+            ncol = len(cols)
+            if datevalue < 141018:
+                if (ncol - 3) % 5 > 0:
+                    logger.warning('File {0} line{1}: Incorrect number of columns ({2})'.format(datevalue, i, ncol))
+                else:
+                    # first 3: ca/units/scp, every 5: daten/timen/descn/entriesn/exitsn
+                    for j in range(3, ncol, 5):
+                        row = processRow(datevalue, cols, i, j, self._jobname)
+                        if row:
+                            rows.append(row)
+            else:
+                if ncol != 11:
+                    logger.warning('File {0} line{1}: Incorrect number of columns ({2})'.format(datevalue, i, ncol))
+                else:               
+                    # skip column 3,4,5 of station, linename, division
+                    row = processRow(datevalue, cols, i, 6, self._jobname)
+                    if row:
+                        rows.append(row)
+        labels = ['ca', 'unit', 'scp', 'timestamp', 'description', 'entry', 'exit']
+        df = pd.DataFrame.from_records(rows, columns=labels)
+        logger.info('Finish processing: File {0}'.format(datevalue))
+        return df
+
     def run(self):
         """execution phase based on parameters"""
         # get data location
-        files = getDataAddress(self.start_date, self.end_date, self._jobname, self.input_path)
+        files = getDataAddress(self._start_date, self._end_date, self._jobname, self.input_path)
         # configure database
         ### placeholder
-        
+        # process files
+        for f in files:
+            df = self._processFile(f)
+            print(df.head(3))
 
 
-def getDataAddress(int_start_date, int_end_date, prefix, input_path=None):
-    logger = logging.getLogger(prefix)
+def getDataAddress(start_date, end_date, prefix, input_path=None):
     """get data urls or paths"""
-    urls = []
-    data_regex = re.compile(r'^data.*?txt$')
+    logger = logging.getLogger(prefix)
+    data_regex = re.compile(r'turnstile.*?txt$')
     if input_path:
         urls = filter(data_regex.search, os.listdir(input_path)) # local
     else:
         soup = BeautifulSoup(urlopen(URL + "turnstile.html"), "lxml")
         urls = [u['href'] for u in soup.find_all('a', href=data_regex)] # web
-    filter_urls = [u for u in urls if parseDate(u) >= int_start_date and parseDate(u) <= int_end_date]
+    if (prefix == 'clean') and (start_date is None) and (end_date is None):
+        filter_urls = list(urls)
+    else:
+        filter_urls = [u for u in urls if parseDate(u) >= start_date and parseDate(u) <= end_date]
     if not filter_urls:
-        logger.error('No data files found within time window')
+        logger.error('Complete: No data files found within time window.')
         sys.exit(1)
     logger.info('{} available data files within time window'.format(len(filter_urls)))
     return filter_urls
 
 
-def processData(file, prefix):
-    """process downloaded data files
-        a. check # columns
-        b. combine date and time column, convert type to datetime
-        c. convert entry/exit to integer type
-        d. return pandas dataframe
-    """
-    logger = logging.getLogger(prefix)
-    datevalue = parseDate(file)
-    filename = os.path.split(file)[-1]
-    with open(file, 'rb') as f:
-        data = f.read().split('\n')
-    rows = []
-    for i, line in enumerate(data):
-        # post 141018, data file has header, skip first row
-        if datevalue >= 141018 and i == 0:   
-            continue
-        line = line.replace('\x00', '')
-        cols = line.strip().split(',')
-        ncol = len(cols)
-        if datevalue < 141018:
-            if (ncol - 3) % 5 > 0:
-                logger.warning('File {0} line{1}: Incorrect number of columns ({3})'.format(filename, i, ncol))
-                continue               
-            # first 3: ca/units/scp, every 5: daten/timen/descn/entriesn/exitsn
-            for j in range(3, ncol, 5):
-                row = processRow(filename, cols, i, j, prefix)
-                if row:
-                    rows.append(row)
-                else:
-                    continue
-        else:
-            if ncol != 11:
-                logger.warning('File {0} line{1}: Incorrect number of columns ({3})'.format(filename, i, ncol))
-                continue                 
-            # skip column 3,4,5 of station, linename, division
-            row = processRow(filename, cols, i, 6, logger)
-            if row:
-                rows.append(row)
-            else:
-                continue
-    labels = ['ca', 'unit', 'scp', 'timestamp', 'description', 'entry', 'exit']
-    df = pd.DataFrame.from_records(rows, columns=labels)
-    return df
-
-
 def processRow(filename, cols, i, j, prefix):
+    """process single row, return False whenever encounter error"""
     logger = logging.getLogger(prefix)
     timestamp = cols[j] + " " + cols[j+1]
     try:
@@ -243,12 +250,15 @@ def processRow(filename, cols, i, j, prefix):
         entry = int(cols[j+3])
         exit = int(cols[j+4])
     except TypeError:
-        logging.debug('File {0} line {1} column {2},{3}: Incorrect int format ({4},{5})'.format(filename, i, j+3, j+4, entry, exit))
+        logging.warning('File {0} line {1} column {2},{3}: Incorrect int format ({4},{5})'.format(filename, i, j+3, j+4, entry, exit))
         return False
-    return tuple(cols[:3] + [description, entry, exit])
+    return tuple(cols[:3] + [timestamp, description, entry, exit])
 
 
 if __name__ == "__main__":
-    download = Downloader(date_range=("2014-08-01", "2014-08-10"))
-    data_paths = download.run()
+    #download = Downloader(date_range=("2014-08-01", "2014-08-10"))
+    #data_paths = download.run()
+    clean = Cleaner(input_path='./data/download-20181031141109')
+    clean.run()
     print("Example complete.")
+
